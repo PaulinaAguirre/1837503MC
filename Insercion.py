f015b42065ed9407e17aea1680941fabf4a0a882
Paulina Aguirre García <30839982+PaulinaAguirre@users.noreply.github.com>
2017-09-05 05:37:24 -0500

    Add files via upload

---
+++ Insercion.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 05:30:00 2017
+
+@author: Aguir
+"""
+
+#Insercion
+
+def orden_por_insercion(arr):
+    for indice in range(1,len(arr)):
+        valor=arr[indice] #valor es el elemento que vamos a comparar 
+        i=indice-1        #i es el valor anterior al elemento que estamos comparar
+        while i>=0:
+            if (valor<arr[i]): 
+                arr[i+1]=arr[i]
+                arr[i]=valor
+                i=i-1
+            else:
+                break
+    return arr
+    
