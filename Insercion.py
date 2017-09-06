e27e06d3b7ffd7c67b9f0d59907ae685baaa770f
Paulina Aguirre García <30839982+PaulinaAguirre@users.noreply.github.com>
2017-09-06 11:29:36 -0500

    CNT
    Añadí contador

--- Insercion.py
+++ Insercion.py
@@ -7,11 +7,14 @@ Created on Tue Sep  5 05:30:00 2017
 
 #Insercion
 
+cnt=0
 def orden_por_insercion(arr):
+    global cnt
     for indice in range(1,len(arr)):
         valor=arr[indice] #valor es el elemento que vamos a comparar 
         i=indice-1        #i es el valor anterior al elemento que estamos comparar
         while i>=0:
+            cnt+=1
             if (valor<arr[i]): 
                 arr[i+1]=arr[i]
                 arr[i]=valor
